981d9a724210388b7cb7be6390a1c9c74d33fee7
albr_mh <albrecht.michael1989@gmail.com>
2019-01-07 17:15:48 +0100

    just a save

--- Day6.py
+++ Day6.py
@@ -1,2 +1,18 @@
 from Day2 import getPuzzleinput
 
+def borders(coordinates):
+    x=[]
+    y=[]
+    for l in coordinates:
+        i,v = l.split(', ')
+        x.append(int(i))
+        y.append(int(v))
+    return max(x),max(y)
+
+
+
+
+if __name__=="__main__":
+
+    coordinates=getPuzzleinput(6)
+    X,Y=borders(coordinates)
