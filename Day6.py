19bb45009f80cee41167c1a50e47a622a8bc11ca
albr_mh <albrecht.michael1989@gmail.com>
2019-01-08 17:24:41 +0100

    just a save

--- Day6.py
+++ Day6.py
@@ -1,15 +1,19 @@
 from Day2 import getPuzzleinput
+import numpy as np
 
 def borders(coordinates):
     x=[]
     y=[]
+    coordinates={l:(int(i),int(v)) for l in}
     for l in coordinates:
         i,v = l.split(', ')
         x.append(int(i))
         y.append(int(v))
+        coordinates[l]={L:(int(i),int(v))}
     return max(x),max(y)
 
-
+def manhatten(x,y,*points):
+    grid=np.zeros((x,y),dtype=int)
 
 
 if __name__=="__main__":
