557bdd0a9f2324a5d747a056e2c51b6cdc171b0e
albr_mh <albrecht.michael1989@gmail.com>
2019-01-14 18:08:59 +0100

    just a save

--- Day6.py
+++ Day6.py
@@ -8,28 +8,66 @@ def borders(coordinates):
     maxY=(max(points[key][1] for key in points))
     return points,maxX,maxY
 
-def distance(x1,y1,x2,y2):
+def meshgrid(x,y):
+    x_ax=np.arange(x)
+    y_ax=np.arange(y)
+    xx,yy=np.meshgrid(x_ax, y_ax)
+    coords=np.dstack([xx,yy]).reshape(-1,2)
+    return coords
 
-    d=abs(x1-x2)+abs(y1-y2)
+def distance(p1,p2):
+
+    d=abs(p1[0]-p2[0])+abs(p1[1]-p2[1])
     return d
 
-def manhatten(x,y,points):
+def manhatten(grid,points):
+    dist=np.zeros((len(grid),2))
+    gridndist=np.concatenate((grid,dist),axis=1)
+    for p in points:
+        #g = np.nditer(grid, flags=['f_index'], op_flags=['readwrite'])
+        for i in range(len(gridndist)):
+            #print(i[0])
+            d=distance(points[p],(gridndist[i][0],gridndist[i][1]))
+            if p==0:
+                gridndist[i][2] = p
+                gridndist[i][3] = d
+            elif gridndist[i][3]>d:
+                gridndist[i][3]=d
+                gridndist[i][2]=p
+            elif gridndist[i][3]==d:
+                gridndist[i][2] = 99
+            else:
+                pass
+    infinite=[]
+    for i in range(len(gridndist)):
+        if (gridndist[i][0]==0 or gridndist[i][1]==0) and (gridndist[i][2] not in infinite):
+            infinite.append(gridndist[i][2])
 
-    grid=np.zeros((x,y),dtype=int)
-    for i,(v,s) in points.items():
-        grid[v-1][s-1]=i+1000
-        if i == 0:
-            for a in np.nditer(grid, flags=['multi_index'], op_flags=['readwrite']):
-                a[...] = i
-        else:
-            a = np.nditer(grid, flags=['multi_index'], op_flags=['readwrite'])
-            for k in a:
-                print(a[0], a.multi_index)
-                #print(type(a.multi_index))
-                try:
-                    a.__next__()
-                except:
-                    break
+    for i in infinite:
+        try:
+            del points[int(i)]
+        except:
+            pass
+    pointsum=[]
+
+    for p in points:
+        pointsum.append(np.count_nonzero(gridndist[:,2]==p))
+
+    # grid=np.zeros((x,y),dtype=int)
+    # for i,(v,s) in points.items():
+    #     grid[v-1][s-1]=i+1000
+    #     if i == 0:
+    #         for a in np.nditer(grid, flags=['multi_index'], op_flags=['readwrite']):
+    #             a[...] = i
+    #     else:
+    #         a = np.nditer(grid, flags=['multi_index'], op_flags=['readwrite'])
+    #         for k in a:
+    #             print(a[0], a.multi_index)
+    #             #print(type(a.multi_index))
+    #             try:
+    #                 a.__next__()
+    #             except:
+    #                 break
 
 
         # elif
@@ -43,10 +81,11 @@ def manhatten(x,y,points):
         #         a[...]=
 
 
-    return grid
+    return gridndist,infinite,pointsum
 
 if __name__=="__main__":
 
     coordinates=getPuzzleinput(6)
     points,x,y=borders(coordinates)
-    catch=manhatten(x,y,points)
+    grid=meshgrid(x,y)
+    gd,inf,res=manhatten(grid,points)
--- Schmierfile.py
+++ Schmierfile.py
@@ -31,9 +31,9 @@ stinglist =['zihrtxgmwcfpbunoleydukjmqv','zihrtxgmwcfpbunoleydukjmqk']
 #[int(s) for s in str.split() if s.isdigit()]#
 
 test1=[1,2,3,5,6,7,8,9]
-# test2=[7,8,9]
-# for i,j in itertools(test1,test2)):
-#     print (i,j)
+test2=[7,8,9]
+for i in test1 for k in test2:
+    print (i,j)
 
 
 # X = np.zeros((100, 200, 300))
@@ -43,15 +43,15 @@ test1=[1,2,3,5,6,7,8,9]
 # for i,s in it.product(range(30, 52), range(1, 18)):
 #     print(i,s)
 
-def notlast(itr):
-    itr = iter(itr)  # ensure we have an iterator
-    prev = itr.__next__()
-    for item in itr:
-        yield prev
-        prev = item
-
-meiniterator=notlast(test1)
-
-for i in meiniterator:
-    print(i)
+# def notlast(itr):
+#     itr = iter(itr)  # ensure we have an iterator
+#     prev = itr.__next__()
+#     for item in itr:
+#         yield prev
+#         prev = item
+#
+# meiniterator=notlast(test1)
+#
+# for i in meiniterator:
+#     print(i)
 
