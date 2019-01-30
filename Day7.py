852bdddba5685a83368f96700941912c0f56df53
Baer <albrecht.michael1989@gmail.com>
2019-01-31 00:34:39 +0100

    Just a Safe

--- Day7.py
+++ Day7.py
@@ -1,39 +1,53 @@
 from Day2 import getPuzzleinput
 import numpy as np
+import itertools
+import collections
 
 def proneInput(Inp):
     return [(x.split(' ')[1], x.split(' ')[-3])for x in Inp]
+#
+#
+def presort(Inp):
+    d=collections.defaultdict(list)
+    for k,v in Inp:
+        d[k].append(v)
+        d[k].sort()
 
+    return d
 
-def order(Inp):
-    letter=['abcdefghijklmnopqrstuvwxyz']
+def work(Inp):
     manual=[]
+#------------------------start poin and queue------------------------------------
+    start=[i for i in Inp.keys() if not any(i in x for x in Inp.values())]
+    start.sort()
+    startval=start[0]
+    queue=start[1:]
+#---------------------------working---------------------------------------------
+    InpCop = dict(Inp)
+
+    while InpCop or queue:
+        manual.append(startval)
+        try:
+            queue.extend(InpCop[startval])
+            del InpCop[startval]
+        except KeyError:
+            pass
+        queue=sorted(set(queue))
+        queue=[x for x in queue if x not in manual]
+        try:
+            startval=queue[0]
+            queue=queue[1:]
+        except IndexError:
+            pass
+
+    return ''.join(manual)
 
 
-    it=0
-    while Inp:
-
-        if it==0:
-            manual.append(Inp[0][0])
-            stage = Inp[0][1]
-            del Inp[0]
-
-        nexts = []
-        dellist=[]
-        for ind, befaft in enumerate(Inp):
-            if befaft[0] == stage or  befaft[0] in stage:
-                nexts.append(befaft[1])
-                dellist.append(ind)
-        manual.extend(sorted(nexts))
-        stage=nexts
-        for n in sorted(dellist,reverse=True):
-            del(Inp[n])
-
-        it += 1
 
 
 
-    return manual
+
+
 
 
 
@@ -42,4 +56,6 @@ if __name__=="__main__":
 
     Inp=getPuzzleinput(7)
     pronedInp=proneInput(Inp)
-    res=order(pronedInp)
+    res=presort(pronedInp)
+    man=work(res)
+
