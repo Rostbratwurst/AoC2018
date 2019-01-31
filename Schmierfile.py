ef4db84ea954be84ab18f5dcf002fa89b61c7857
Baer <albrecht.michael1989@gmail.com>
2019-02-01 00:27:55 +0100

    Just a Safe

--- Schmierfile.py
+++ Schmierfile.py
@@ -55,32 +55,67 @@ stinglist =['zihrtxgmwcfpbunoleydukjmqv','zihrtxgmwcfpbunoleydukjmqk']
 # for i in meiniterator:
 #     print(i)
 from collections import defaultdict, deque
-from Day2 import getPuzzleinput
- # Edges
+
+# Edges
 E = defaultdict(list)
- # In-degree
+# In-degree
 D = defaultdict(int)
-Inp=getPuzzleinput(7)
-for line in Inp:
+for line in open('input_day7.txt'):
     words = line.split()
     x = words[1]
     y = words[7]
     E[x].append(y)
     D[y] += 1
 
-Q=[]
+for k in E:
+    E[k] = sorted(E[k])
+
+# time
+t = 0
+# Events
+EV = []
+# Work queue
+Q = []
+
+
+def add_task(x):
+    Q.append(x)
+
+
+def start_work():
+    global Q
+    while len(EV) < 5 and Q:
+        x = min(Q)
+        Q = [y for y in Q if y != x]
+        print('Starting {} at {}'.format(x, t))
+        EV.append((t + 61 + ord(x) - ord('A'), x))
+
+
 for k in E:
     if D[k] == 0:
-        Q.append(k)
+        add_task(k)
+start_work()
 
-
-ans = ""
-while Q:
-    x = sorted(Q)[0]
-    Q = [y for y in Q if y!=x]
-    ans += x
+while EV or Q:
+    t, x = min(EV)
+    print(t, x)
+    EV = [y for y in EV if y != (t, x)]
     for y in E[x]:
         D[y] -= 1
         if D[y] == 0:
-            Q.append(y)
-print (ans)
+            add_task(y)
+    start_work()
+
+print(t)
+
+# ans = ""
+# while Q:
+#    x = sorted(Q)[0]
+#    Q = [y for y in Q if y!=x]
+#    ans += x
+#    for y in E[x]:
+#        D[y] -= 1
+#        if D[y] == 0:
+#            Q.append(y)
+# print ans
+#
